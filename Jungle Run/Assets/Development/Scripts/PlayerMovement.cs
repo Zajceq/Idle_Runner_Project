f9c5f4f1abf2f135b7148a102179b5cb92e53d33
UnityGuide <zajceq3@gmail.com>
2024-05-03 21:20:51 +0200

    Added new input actions and player movement

--- Jungle Run/Assets/Development/Scripts/PlayerMovement.cs
+++ Jungle Run/Assets/Development/Scripts/PlayerMovement.cs
@@ -1,22 +1,59 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 5.0f;
+    [SerializeField] private float jumpForce = 15.0f;
 
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private InputActionReference moveAction;
+    [SerializeField] private InputActionReference jumpAction;
+
+    private Rigidbody2D _rigidbody;
+    private float _playersMovementDirection = 0;
+
+    private void Awake()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        float horizontalInput = Input.GetAxis("Horizontal");
-        Vector3 movement = new Vector3(horizontalInput, 0, 0);
-        transform.Translate(movement * speed * Time.deltaTime);
+        // enable and subscribe actions so the player will respond on input
+        moveAction.action.Enable();
+        jumpAction.action.Enable();
+
+        moveAction.action.performed += OnMove;
+        jumpAction.action.performed += OnJump;
+    }
+
+    private void OnDisable()
+    {
+        // unsubscribe and disable actions OnDisable to prevent bugs
+        moveAction.action.performed -= OnMove;
+        jumpAction.action.performed -= OnJump;
+
+        moveAction.action.Disable();
+        jumpAction.action.Disable();
+    }
+
+    private void OnMove(InputAction.CallbackContext context)
+    {
+        _playersMovementDirection = context.ReadValue<float>();
+    }
+
+    private void OnJump(InputAction.CallbackContext context)
+    {
+        // Handle jumping
+        if (Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        {
+            _rigidbody.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        // Apply horizontal movement
+        _rigidbody.velocity = new Vector2(_playersMovementDirection * speed, _rigidbody.velocity.y);
     }
 }
